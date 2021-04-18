5f9389baef8c5b93f3cceea3ba3c79ba121bd46c
Mohmaed hany <medo72030@gmail.com>
2021-04-18 07:22:09 +0200

    first student commit

---
+++ student.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+struct student
+{
+    char name[50];
+    int id;
+    int day_of_birth;
+    int month_of_birth;
+    int year_of_birth;
+    int score;
+};
+int main()
+{
+    printf("Welcome to our program!");
+
+    return 0;
+}
